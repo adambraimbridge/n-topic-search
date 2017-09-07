a2cea6537dea075c48d618cdc32998c86cca0db8
Gabrielle von Koss <gabivk@gmail.com>
2017-09-07 10:18:08 +0100

    update host because CORS also

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Delegate = require('ftdomdelegate');
 import { debounce } from 'n-ui-foundations';
 import suggestionList from './src/renderers/search-suggestions';
+const host = /local(?:host)?\.ft\.com/.test(window.location.host) ? window.location.host : 'www.ft.com';
 
 function getNonMatcher (container) {
 	if (typeof container === 'string') {
@@ -33,7 +34,7 @@ class TopicSearch {
 		this.listComponent = listComponent;
 		this.preSuggest = preSuggest;
 		this.searchEl = this.container.querySelector('[data-n-topic-search-input]');
-		this.dataSrc = `//${window.location.host}/search-api/suggestions?partial=`;
+		this.dataSrc = `//${host}/search-api/suggestions?partial=`;
 		this.categories = (this.container.getAttribute('data-n-topic-search-categories') || 'tags').split(',');
 		this.itemTag = this.container.getAttribute('data-n-topic-search-item-tag') || 'a';
 		this.includeViewAllLink = this.container.hasAttribute('data-n-topic-search-view-all');
